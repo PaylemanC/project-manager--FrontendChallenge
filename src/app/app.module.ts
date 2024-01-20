6102231ff1036190c360a667cac3a4e4bb8fdf05
Carina Payleman <rociocarina2@gmail.com>
2024-01-19 21:09:51 -0300

    Split header & subheader, new folder components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { SubheaderComponent } from './components/subheader/subheader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    SubheaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/subheader/subheader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subheader',
+  templateUrl: './subheader.component.html',
+  styleUrls: ['./subheader.component.scss']
+})
+export class SubheaderComponent {
+
+}

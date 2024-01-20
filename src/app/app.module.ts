00e7c93ecb6bb02008f6fb7d582436cd185cbfa9
Carina Payleman <rociocarina2@gmail.com>
2024-01-20 01:07:05 -0300

    Create static form create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SubheaderComponent } from './components/subheader/subheader.component';
 import { ProjectsListComponent } from './pages/projects-list/projects-list.component';
 import { NewProjectComponent } from './pages/new-project/new-project.component';
+import { ProjectFormComponent } from './components/project-form/project-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SubheaderComponent,
     ProjectsListComponent,
-    NewProjectComponent
+    NewProjectComponent,
+    ProjectFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/project-form/project-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-form',
+  templateUrl: './project-form.component.html',
+  styleUrls: ['./project-form.component.scss']
+})
+export class ProjectFormComponent {
+
+}
